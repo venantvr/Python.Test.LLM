6fc4f3c01d718983d79a042fb3fcaf026da4836b
venantvr <r.venant.valery@gmail.com>
2024-11-07 19:17:58 +0100

    First commit...

--- main-05.py
+++ main-05.py
@@ -3,7 +3,9 @@ from gliner import GLiNER
 model = GLiNER.from_pretrained("almanach/camembert-bio-gliner-v0.1")
 
 text = """
-Mme A.P. âgée de 52 ans, non tabagique, ayant un diabète de type 2 a été hospitalisée pour une pneumopathie infectieuse. Cette patiente présentait depuis 2 ans des infections respiratoires traités en ambulatoire. L’examen physique a trouvé une fièvre à 38ºc et un foyer de râles crépitants de la base pulmonaire droite.
+Mme A.P. âgée de 52 ans, non tabagique, ayant un diabète de type 2 a été hospitalisée pour une 
+pneumopathie infectieuse. Cette patiente présentait depuis 2 ans des infections respiratoires traités en ambulatoire. 
+L’examen physique a trouvé une fièvre à 38ºc et un foyer de râles crépitants de la base pulmonaire droite.
 """
 
 labels = ["Âge", "Patient", "Maladie", "Symptômes"]
