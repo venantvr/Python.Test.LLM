adee74272e38f59f26f65c6541f6cb0a7e6803c7
venantvr <r.venant.valery@gmail.com>
2024-11-07 11:36:56 +0100

    First commit...

--- main-01.py
+++ main-01.py
@@ -1,20 +1,20 @@
 from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
 
-# Charger distilBERT pour la reconnaissance d'entités nommées (NER)
-model_name = "distilbert-base-cased"
-model = AutoModelForTokenClassification.from_pretrained("dbmdz/distilbert-base-cased-finetuned-conll03-english")
-tokenizer = AutoTokenizer.from_pretrained("dbmdz/distilbert-base-cased-finetuned-conll03-english")
+# Charger le modèle Camembert pour la reconnaissance d'entités nommées (NER)
+model_name = "Jean-Baptiste/camembert-ner"
+model = AutoModelForTokenClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Créer un pipeline pour la reconnaissance d'entités nommées (NER)
 nlp_ner = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
 
 
-def anonymize_text_with_distilbert(text):
-    # Utiliser distilBERT pour détecter les entités nommées
+def anonymize_text_with_camembert(text):
+    # Utiliser Camembert pour détecter les entités nommées
     entities = nlp_ner(text)
     anonymized_text = text
     for entity in entities:
-        # Remplacer chaque entité détectée par son type (par exemple, PERSON, LOCATION)
+        # Remplacer chaque entité détectée par son type
         anonymized_text = anonymized_text.replace(entity['word'], f"<{entity['entity_group']}>")
 
     return anonymized_text
@@ -22,7 +22,7 @@ def anonymize_text_with_distilbert(text):
 
 # Exemple de texte à anonymiser
 text = "Jean Dupont habite à Paris depuis 2021. Son numéro de téléphone est 0123456789."
-anonymized_text = anonymize_text_with_distilbert(text)
+anonymized_text = anonymize_text_with_camembert(text)
 
 print("Texte original :", text)
 print("Texte anonymisé :", anonymized_text)
