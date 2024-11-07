e93817f61636e3a7ce237bf1b28970e65c729a63
venantvr <r.venant.valery@gmail.com>
2024-11-07 18:17:15 +0100

    First commit...

--- main-05.py
+++ main-05.py
@@ -19,6 +19,8 @@ def generate_medical_json(text: str) -> dict:
     inputs = tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True)
     outputs = model.generate(inputs["input_ids"], max_length=256, num_beams=5, early_stopping=True)
 
+    print(outputs[0])
+
     # Décoder et structurer la réponse
     json_response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
