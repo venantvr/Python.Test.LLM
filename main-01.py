570acdae498328196d4f538d05e15d2d4560b0a3
venantvr <r.venant.valery@gmail.com>
2024-11-07 11:40:53 +0100

    First commit...

--- main-01.py
+++ main-01.py
@@ -3,14 +3,23 @@ from transformers import pipeline
 # Utiliser le modèle Camembert standard pour le NER
 nlp_ner = pipeline("ner", model="camembert-base", tokenizer="camembert-base", aggregation_strategy="simple")
 
+# Mapping de labels pour des noms plus explicites (selon les besoins du modèle)
+label_mapping = {
+    "LABEL_0": "PERSONNE",
+    "LABEL_1": "LIEU",
+    "LABEL_2": "ORGANISATION",
+    "LABEL_3": "DATE",
+}
+
 
 def anonymize_text_with_camembert(text):
     # Utiliser Camembert pour détecter les entités nommées
     entities = nlp_ner(text)
     anonymized_text = text
     for entity in entities:
-        # Remplacer chaque entité détectée par son type
-        anonymized_text = anonymized_text.replace(entity['word'], f"<{entity['entity_group']}>")
+        # Utiliser le mapping des labels pour remplacer chaque entité détectée
+        label = label_mapping.get(entity['entity'], "ENTITE")
+        anonymized_text = anonymized_text.replace(entity['word'], f"<{label}>")
 
     return anonymized_text
 
