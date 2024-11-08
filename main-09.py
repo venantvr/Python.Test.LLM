2e5b3c82992a9736b3e79b5e87d286b32184b7e8
venantvr <r.venant.valery@gmail.com>
2024-11-08 16:06:39 +0100

    First commit...

--- main-09.py
+++ main-09.py
@@ -1,14 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 
-# Charger le tokenizer et le modèle AliBERT
-tokenizer = AutoTokenizer.from_pretrained("AliBERT")
-model = AutoModelForTokenClassification.from_pretrained("AliBERT")
+# Charger le modèle BioBERT pour la NER biomédicale
+model_name = "dmis-lab/biobert-base-cased-v1.1"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
 
 # Créer une pipeline NER
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
 
-# Texte médical à analyser
+# Texte médical à analyser (en français)
 text = """
 Mme A.P., âgée de 52 ans, non fumeuse, ayant un diabète de type 2, a été hospitalisée pour une pneumopathie infectieuse.
 """
